5b3fe59dc29a2621c6e8b528043650aa312b8625
Fabio Andrei Lorentz <tifabio@gmail.com>
2021-02-13 13:40:38 -0300

    Notificationss migration

---
+++ database/migrations/2021_02_13_163604_create_notifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('content');
+            $table->timestamps();
+
+            $table->foreignId('notifications_status_id')->constrained('notifications_status', 'id');
+            $table->foreignId('users_id')->constrained('users', 'id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}

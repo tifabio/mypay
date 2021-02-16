1b36ecf6f66b9c0fa3bce930689fb2445eaa807d
Fabio Andrei Lorentz <tifabio@gmail.com>
2021-02-16 11:33:13 -0300

    Improving tests

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,4 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'document',
+        'email',
+        'password',
+        'balance',
+        'user_types_id'
+    ];
 }
--- tests/TransferTest.php
+++ tests/TransferTest.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Models\User;
+use App\Models\UserType;
+use App\Repositories\UserRepository;
 use App\Services\TransferService;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Lumen\Testing\DatabaseTransactions;
 
 class TransferTest extends TestCase
@@ -10,93 +12,159 @@ class TransferTest extends TestCase
     use DatabaseTransactions;
 
     /**
-     * @var User $userModel
+     * @var UserRepository $userRepository
      */
-    private $userModel;
+    private $userRepository;
 
     /**
      * @var TransferService $transferService
      */
     private $transferService;
 
+    /**
+     * @var array $user
+     */
+    private $user;
+
+    /**
+     * @var array $shopkeeper
+     */
+    private $shopkeeper;
+
     public function setUp(): void
     {
         parent::setUp();
-        $this->userModel = $this->app->make(User::class);
+        $this->userRepository = $this->app->make(UserRepository::class);
         $this->transferService = $this->app->make(TransferService::class);
+
+        $this->user = [
+            'name' => 'Joao',
+            'document' => '66900839002',
+            'email' => 'joao@teste.dev',
+            'password' => Hash::make('joao@123'),
+            'balance' => 500.00,
+            'user_types_id' => UserType::TYPE_USER
+        ];
+
+        $this->shopkeeper = [
+            'name' => 'Pedro',
+            'document' => '29268732000149',
+            'email' => 'pedro@teste.dev',
+            'password' => Hash::make('pedro@123'),
+            'balance' => 50.00,
+            'user_types_id' => UserType::TYPE_SHOPKEEPER
+        ];
     }
 
     /**
      * Test if shopkeeper can't transfer
-     * Users created by seed using fakeUsers (from UsersRepository)
      */
     public function testUserCanNotTransfer()
     {
+        $payer = $this->userRepository->save($this->shopkeeper);
+        $payee = $this->userRepository->save($this->user);
+
         $this->json('POST', route('transfer.create'), [
-            'value' => '100',
-            'payer' => '3',
-            'payee' => '1'
-        ])
-        ->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+            'value' => 100,
+            'payer' => $payer->id,
+            'payee' => $payee->id
+        ])->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     /**
      * Test if user has balance
-     * Users created by seed using fakeUsers (from UsersRepository)
      */
     public function testUserHasBalance()
     {
+        $payer = $this->userRepository->save($this->user);
+        $payee = $this->userRepository->save($this->shopkeeper);
+
         $this->json('POST', route('transfer.create'), [
-            'value' => '600',
-            'payer' => '1',
-            'payee' => '2'
-        ])
-        ->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+            'value' => 600,
+            'payer' => $payer->id,
+            'payee' => $payee->id
+        ])->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * Test invalid value
+     */
+    public function testInvalidValue()
+    {
+        $payer = $this->userRepository->save($this->user);
+        $payee = $this->userRepository->save($this->shopkeeper);
+
+        $this->json('POST', route('transfer.create'), [
+            'value' => -50,
+            'payer' => $payer->id,
+            'payee' => $payee->id
+        ])->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * Test required fields
+     */
+    public function testRequiredFields()
+    {
+        $payer = $this->userRepository->save($this->user);
+        $payee = $this->userRepository->save($this->shopkeeper);
+
+        $this->json('POST', route('transfer.create'), [
+            'payer' => $payer->id,
+            'payee' => $payee->id
+        ])->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+        $this->json('POST', route('transfer.create'), [
+            'value' => -50,
+            'payee' => $payee->id
+        ])->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
+
+
+        $this->json('POST', route('transfer.create'), [
+            'value' => -50,
+            'payer' => $payer->id,
+        ])->seeStatusCode(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 
     /**
      * Test valid transfer
-     * Users created by seed using fakeUsers (from UsersRepository)
      */
     public function testValidTransfer()
     {
-        $payer_id = 1;
-        $payee_id = 2;
+        $payer = $this->userRepository->save($this->user);
+        $payee = $this->userRepository->save($this->shopkeeper);
 
         $this->json('POST', route('transfer.create'), [
-            'value' => '200',
-            'payer' => $payer_id,
-            'payee' => $payee_id
-        ])
-        ->seeStatusCode(Response::HTTP_CREATED);
+            'value' => 200,
+            'payer' => $payer->id,
+            'payee' => $payee->id
+        ])->seeStatusCode(Response::HTTP_CREATED);
 
-        $this->assertEquals(300, $this->userModel->find($payer_id)->balance);
-        $this->assertEquals(250, $this->userModel->find($payee_id)->balance);
+        $this->assertEquals(300, $this->userRepository->find($payer->id)->balance);
+        $this->assertEquals(250, $this->userRepository->find($payee->id)->balance);
     }
 
     /**
      * Test revert transfer
-     * Users created by seed using fakeUsers (from UsersRepository)
      */
     public function testRevertTransfer()
     {
-        $payer_id = 1;
-        $payee_id = 2;
+        $payer = $this->userRepository->save($this->user);
+        $payee = $this->userRepository->save($this->shopkeeper);
 
         $this->json('POST', route('transfer.create'), [
-            'value' => '200',
-            'payer' => $payer_id,
-            'payee' => $payee_id
-        ])
-        ->seeStatusCode(Response::HTTP_CREATED);
+            'value' => 200,
+            'payer' => $payer->id,
+            'payee' => $payee->id
+        ])->seeStatusCode(Response::HTTP_CREATED);
 
-        $this->assertEquals(300, $this->userModel->find($payer_id)->balance);
-        $this->assertEquals(250, $this->userModel->find($payee_id)->balance);
+        $this->assertEquals(300, $this->userRepository->find($payer->id)->balance);
+        $this->assertEquals(250, $this->userRepository->find($payee->id)->balance);
 
         $response = json_decode($this->response->getContent());
         $this->transferService->revert($response->id);
 
-        $this->assertEquals(500, $this->userModel->find($payer_id)->balance);
-        $this->assertEquals(50, $this->userModel->find($payee_id)->balance);
+        $this->assertEquals(500, $this->userRepository->find($payer->id)->balance);
+        $this->assertEquals(50, $this->userRepository->find($payee->id)->balance);
     }
 }
